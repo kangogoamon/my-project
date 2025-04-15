268a4b0b6ebcbe4f56abe3c0cef1f3383b89732f
kangogoamon <47350890+kangogoamon@users.noreply.github.com>
2025-04-15 12:06:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 function displayGreeting() {
     const hour = new Date().getHours();
     let greeting;
-    if (hour < 12) greeting = "Good morning";
-    else if (hour < 18) greeting = "Good afternoon";
-    else greeting = "Good evening";
+    if (hour < 12) greeting = "Good morning!";
+    else if (hour < 18) greeting = "Good afternoon!";
+    else greeting = "Good evening!";
     document.getElementById('greeting').textContent = greeting;
 }
 
